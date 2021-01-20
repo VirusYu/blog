b22a82c36a4d55c6f7bd9d2ed210853197cf4b18
于诚信 <ergou@uxin.com>
2021-01-20 21:01:41 +0800

    打包

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,9 @@ module.exports = {
   title: '二狗',
   theme: 'reco',
   description: '不忘初心，方得始终',
-  head: [[('link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/image/dog.png' })], ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]],
+  head: [[('link', { rel: 'shortcut icon', href: '/image/dog.png' })], ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]],
   themeConfig: {
     type: 'blog',
-    subSidebar: 'auto',
     authorAvatar: '/image/avatar.jpeg',
     // 博客配置
     blogConfig: {
@@ -21,11 +20,6 @@ module.exports = {
         // 信息栏展示社交信息
         { icon: 'reco-github', link: 'https://github.com/VirusYu' },
       ],
-      nav: [{ text: '时间轴', link: '/timeline/', icon: 'reco-date' }],
-      record: 'ICP 备案文案',
-      recordLink: 'ICP 备案指向链接',
-      cyberSecurityRecord: '公安部备案文案',
-      cyberSecurityLink: '公安部备案指向链接',
       // 项目开始时间，只填写年份
       startYear: '2021',
       author: '二狗',
