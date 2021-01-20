9be75576eb8f3e1dc5deaebe0c7d52e57ddad603
于诚信 <ergou@uxin.com>
2021-01-20 20:37:08 +0800

    首页初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,34 @@
 module.exports = {
-  title: '二狗的博客',
+  title: '二狗',
   theme: 'reco',
-  description: '技术分享和生活小记',
-  head: [[('link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/image/dog.png' })]],
+  description: '不忘初心，方得始终',
+  head: [[('link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/image/dog.png' })], ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]],
+  themeConfig: {
+    type: 'blog',
+    subSidebar: 'auto',
+    authorAvatar: '/image/avatar.jpeg',
+    // 博客配置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: '分类', // 默认文案 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: '标签', // 默认文案 “标签”
+      },
+      socialLinks: [
+        // 信息栏展示社交信息
+        { icon: 'reco-github', link: 'https://github.com/VirusYu' },
+      ],
+      nav: [{ text: '时间轴', link: '/timeline/', icon: 'reco-date' }],
+      record: 'ICP 备案文案',
+      recordLink: 'ICP 备案指向链接',
+      cyberSecurityRecord: '公安部备案文案',
+      cyberSecurityLink: '公安部备案指向链接',
+      // 项目开始时间，只填写年份
+      startYear: '2021',
+      author: '二狗',
+    },
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-# 你好，世界
+---
+home: true
+title: 二狗
+date: 2021-01-20
+tags: 首页
+categories: 首页
+heroText: 二狗 の 技术分享 & 生活记事
+heroImage: /image/avatar.jpeg
+heroImageStyle: {
+  maxHeight: '200px',
+  display: block,
+  margin: '6rem auto 1.5rem',
+  borderRadius: '50%',
+  boxShadow: '0 5px 18px rgba(0,0,0,0.2)'
+}
+---
