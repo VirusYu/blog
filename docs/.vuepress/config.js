dfd84d3398bde675c1e6e10d1fb6c60545cea291
于诚信 <ergou@uxin.com>
2021-01-20 19:52:39 +0800

    增加主题

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  title: '二狗的博客',
+  theme: 'reco',
+  description: '技术分享和生活小记',
+  head: [[('link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/image/dog.png' })]],
+}
